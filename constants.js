9e1b661c63e6ac621bd58646b2a4fb0c0835d3b9
Vova Porton <vova@500tech.com>
2017-12-09 13:46:41 +0200

    Added constants file

---
+++ constants.js
@@ -0,0 +1,18 @@
+export const LIST = [
+  'Eggs',
+  'Milk',
+  'Bread',
+  'Juice',
+  'Meat',
+  'Oranges',
+  'Apples',
+  'Kiwis',
+  'Ice cream',
+  'Sand',
+  'Sweater',
+  'iPhone',
+  'Android',
+  'Dell',
+  'Apple',
+  'El-Al'
+];
