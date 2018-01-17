751a9c542622c812d1eb346774e3a9ce7040be55
Vova Porton <vova@500tech.com>
2018-01-17 13:49:17 +0200

    Temp

--- App.js
+++ App.js
@@ -1,64 +1,13 @@
-import React, { Component } from 'react';
-import {
-  StyleSheet,
-  View,
-  Button,
-  Image,
-  ScrollView,
-  Text
-} from 'react-native';
+import { DrawerNavigator } from 'react-navigation';
 
-import { NAMES_URL, AVATARS_URL } from './constants';
+import Users from './Users';
+import Settings from './Settings';
 
-export default class App extends Component<{}> {
-  state = {
-    users: []
-  };
-
-  fetchUsers = () => {
-    fetch(NAMES_URL).then(response => response.json()).then((data) => this.setState({ users: data }));
-  };
-
-  render() {
-    return (
-      <View style={styles.container}>
-        <Button title="Fetch users"
-                onPress={ this.fetchUsers }/>
-
-        <ScrollView style={{ width: '100%' }}>
-          {
-            this.state.users.map(user => (
-              <View key={ user.name }
-                    style={{ padding: 10, flexDirection: 'row', justifyContent: 'space-between' }}>
-                <Text>{ user.name }</Text>
-
-                <Image source={{ uri: AVATARS_URL + user.name }}
-                       style={{ width: 50, height: 50 }}/>
-              </View>
-            ))
-          }
-        </ScrollView>
-      </View>
-    );
+const App = DrawerNavigator({
+  Users: {
+    screen: Users
+  },
+  Settings: {
+    screen: Settings
   }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    paddingTop: 20,
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
 });
---
+++ Settings.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+class Settings extends React.Component {
+  render() {
+    return (
+
+    )
+  }
+};
+
+export default Settings;
